0fd9b75264a489183fb22d0f69e35f89b59659cc
srihari <srihariadada19978@gmail.com>
2024-10-09 07:06:05 +0530

    empty file commiting

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        java.lang.System.out.println("Execution will be started from main method by JVM");
+        java.lang.System.out.println("Hello World");
+    }
+}
